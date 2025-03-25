c0e44857b554d6a1819aa8a8f1b66242a00795d6
luisxsssx <cd210902@gmail.com>
2025-03-25 11:50:00 -0700

    CORS config

--- src/main/java/com/back/workout/WorkoutApplication.java
+++ src/main/java/com/back/workout/WorkoutApplication.java
@@ -2,12 +2,30 @@ package com.back.workout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WorkoutApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkoutApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		return new WebMvcConfigurer() {
+
+			public void addCorsMappings(CorsRegistry corsRegistry) {
+				corsRegistry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true)
+						.maxAge(3600);
+			}
+		};
 	}
 
 }
